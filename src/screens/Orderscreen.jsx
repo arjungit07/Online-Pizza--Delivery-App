71de062c6e91b874a075cee0a31e82d53c155145
arjungit07 <139776248+arjungit07@users.noreply.github.com>
2026-05-05 16:08:55 +0530

    Update Orderscreen.jsx

--- src/screens/Orderscreen.jsx
+++ src/screens/Orderscreen.jsx
@@ -1,53 +1,56 @@
-import React,{useEffect} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
-import { getUserOrders } from '../action/orderAction'
-import Loader from '../Components/Loader'
-import { Table } from 'react-bootstrap'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserOrders } from "../action/orderAction";
+import Loader from "../Components/Loader";
+import { Table } from "react-bootstrap";
 
 const Orderscreen = () => {
-
   const currentUser = localStorage.getItem("currentUser")
-  ? JSON.parse(localStorage.getItem("currentUser"))
-  : null;
+    ? JSON.parse(localStorage.getItem("currentUser"))
+    : null;
 
-const userState = useSelector(state => state.getUserOrderReducer)
-const {loading,errors,orders} = userState
+  const userState = useSelector((state) => state.getUserOrderReducer);
+  const { loading, orders } = userState; //  removed errors
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    dispatch(getUserOrders(currentUser.name))
-  }, [dispatch])
+    if (currentUser?.name) {
+      dispatch(getUserOrders(currentUser.name));
+    }
+  }, [dispatch, currentUser?.name]); //  fixed dependency
+
   return (
     <>
-    <h1 style={{textAlign:"center",marginBottom:'30px',}}>Orders</h1>
-    {loading && <Loader/>}
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Order Id</th>
-          <th>Email</th>
-          <th>User</th>
-          <th>Order Amount</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          orders && 
-          orders.map((order)=>(
-            <tr>
-              <td>
-                {order._id}
-              </td>
-              <td>{order.email}</td>
-              <td>{order.name}</td>
-              <td>Rs-/{order.orderAmount}</td>
-            </tr>
-          ))
-        }
-      </tbody>
-    </Table>
-    </>
-  )
-}
+      <h1 style={{ textAlign: "center", marginBottom: "30px" }}>
+        Orders
+      </h1>
 
-export default Orderscreen
+      {loading && <Loader />}
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Order Id</th>
+            <th>Email</th>
+            <th>User</th>
+            <th>Order Amount</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders &&
+            orders.map((order) => (
+              <tr key={order._id}> {/* ✅ key added */}
+                <td>{order._id}</td>
+                <td>{order.email}</td>
+                <td>{order.name}</td>
+                <td>Rs-/{order.orderAmount}</td>
+              </tr>
+            ))}
+        </tbody>
+      </Table>
+    </>
+  );
+};
+
+export default Orderscreen;
