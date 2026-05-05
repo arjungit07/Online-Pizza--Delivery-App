ebc4fc646365d33cfc6dc1bebdaa1f96006c27c6
arjungit07 <139776248+arjungit07@users.noreply.github.com>
2026-05-05 16:10:04 +0530

    Update Register.jsx

--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -1,93 +1,85 @@
-  import React,{useEffect, useState} from 'react'
-  import {Container,Button,Form} from 'react-bootstrap' 
-  import {useDispatch,useSelector} from 'react-redux'
-  import { registerUser } from '../action/userAction'
-  import { useNavigate } from "react-router-dom";
-  // import Loader from '../Components/Loader'
-  // import Success from '../Components/Success'
-  // import Error from '../Components/Error'
+import React, { useState } from "react";
+import { Container, Button, Form } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import { registerUser } from "../action/userAction";
 
-  const Register = () => {
-      const [name,setName] = useState('')
-      const [email,setEmail] = useState('')
-      const [password,setPassword] = useState('')
-      const [confirmPass,setConfirmPass] = useState('')
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPass, setConfirmPass] = useState("");
 
-      // const registerState = useSelector(state => state.userRegisterReducer )
+  const dispatch = useDispatch();
 
-      // const {success} = registerState
+  const registerHandler = () => {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-    const dispatch = useDispatch()
- 
+    if (!email.match(emailPattern)) {
+      alert("Please enter a valid email address");
+      return;
+    }
 
-        const registerHandler = () => {
-          // Regular expression to validate email format
-          const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (password !== confirmPass) {
+      alert("Passwords do not match");
+      return;
+    }
 
-          if (!email.match(emailPattern)) {
-            alert("Please enter a valid email address");
-            return;
-          }
+    const user = { name, password, email, confirmPass };
+    dispatch(registerUser(user));
+  };
 
-          if (password !== confirmPass) {
-            alert("Passwords do not match");
-            return;
-          }
-
-          const user = { name, password, email, confirmPass };
-          dispatch(registerUser(user));
-        };
-    
-
-      
-    return (
-      
-      <>
-      <Container>
+  return (
+    <Container>
       <Form>
-          <h1>Sign Up</h1>
-          <hr />
-          <Form.Group className="mb-3" controlId="formBasicName">
+        <h1>Sign Up</h1>
+        <hr />
+
+        <Form.Group className="mb-3">
           <Form.Label>Name</Form.Label>
-          <Form.Control 
-          type="text" 
-          placeholder="Enter name"
-          value={name} 
-          onChange={e => setName(e.target.value)}/>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
           <Form.Control
-          type="email"
+            type="text"
+            placeholder="Enter name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
             placeholder="Enter email"
-            value={email} 
-            onChange={e => setEmail(e.target.value)} />
-          
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password"
-          value={password} 
-          onChange={e => setPassword(e.target.value)} />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formConfirmBasicPassword">
-          <Form.Label> ConfirmPassword</Form.Label>
-          <Form.Control type="text" placeholder=" Confirm Password"
-          value={confirmPass} 
-          onChange={e => setConfirmPass(e.target.value)} />
+        <Form.Group className="mb-3">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPass}
+            onChange={(e) => setConfirmPass(e.target.value)}
+          />
         </Form.Group>
 
-        <Button variant="primary"
-        onClick={registerHandler}>
+        <Button variant="primary" onClick={registerHandler}>
           Sign Up
         </Button>
-      </Form >
-      </Container>
-      </>
-    )
-  }
+      </Form>
+    </Container>
+  );
+};
 
-  export default Register
+export default Register;
